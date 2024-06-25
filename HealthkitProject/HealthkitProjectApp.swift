9d6c2822ab0257189a8b99f186513f6f529ee975
YurikoAishinselo <yurikoaishinselo123@uvers.ac.id>
2024-06-25 21:06:02 +0700

    Initial Commit

---
+++ HealthkitProject/HealthkitProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthkitProjectApp.swift
+//  HealthkitProject
+//
+//  Created by Yuriko AIshinselo on 25/06/24.
+//
+
+import SwiftUI
+
+@main
+struct HealthkitProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
